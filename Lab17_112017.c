df1f7d84948e76a4305faa05164b7a6ce14b43e2
JJBlazrey <31145560+JJBlazrey@users.noreply.github.com>
2017-11-15 23:37:19 -0600

    Create Lab17_112017.c

---
+++ Lab17_112017.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main(){
+	char string[100];
+	int i=0;
+	FILE *file;
+	file=fopen("archivo.txt", "r");
+	if (file==NULL){
+		printf("No existe el archivo\n");
+		return 0;
+	}
+	printf("El archivo dice: \n");
+	while(!feof(file)){
+		fgets(string,100,file);	
+	}
+	for(i=0;i<strlen(string);i++){
+			string[i]=toupper(string[i]);
+	}
+	printf("\n%s", string);
+	fclose(file);
+	return 0;
+}

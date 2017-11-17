c761749ce519832330e3144c7e87401cb1835239
JJBlazrey <31145560+JJBlazrey@users.noreply.github.com>
2017-11-16 21:14:48 -0600

    Update Lab17_112017.c

--- Lab17_112017.c
+++ Lab17_112017.c
@@ -5,7 +5,7 @@ int main(){
 	char string[100];
 	int i=0;
 	FILE *file;
-	file=fopen("archivo.txt", "r");
+	file=fopen("archivo.txt", "r+");
 	if (file==NULL){
 		printf("No existe el archivo\n");
 		return 0;
@@ -17,7 +17,9 @@ int main(){
 	for(i=0;i<strlen(string);i++){
 			string[i]=toupper(string[i]);
 	}
+	fputs('string', file);
 	printf("\n%s", string);
+	fprintf(file, "\n%s", string);
 	fclose(file);
 	return 0;
 }

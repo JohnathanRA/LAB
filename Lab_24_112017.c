51c7f419b9faa72556051d9dbd24a850e4facd75
JJBlazrey <31145560+JJBlazrey@users.noreply.github.com>
2017-11-24 17:56:14 -0600

    Create Lab_24_112017.c

---
+++ Lab_24_112017.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+int string[100];
+int i=0;
+	int b=0;
+	int c=0;
+	FILE *file;
+	file=fopen("archivo.txt", "r");
+	if (file==NULL){
+		printf("No existe el archivo\n");
+		return 0;
+	}
+	while(!feof(file)){
+		c=fgetc(file);	
+	if(isdigit(c)){
+			b++;	
+	}
+}
+	printf("El total de numeros es: %d \n", b);
+	fclose(file);
+	return 0;	
+}

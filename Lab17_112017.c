ae4b3623231f808f6c771894d41359be24403357
JJBlazrey <31145560+JJBlazrey@users.noreply.github.com>
2017-11-24 18:10:17 -0600

    Update Lab17_112017.c

--- Lab17_112017.c
+++ Lab17_112017.c
@@ -2,28 +2,24 @@
 #include<string.h>
 #include<ctype.h>
 
-int main(){
-int string[100];
-int i=1;
-	int b=0;
-	int c=0;
-	FILE *file;
-	file=fopen("archivo.txt", "r");
-	if (file==NULL){
-		printf("No existe el archivo\n");
-		return 0;
-	}
-	while(!feof(file)){
-		c=fgetc(file);	
-	if(isdigit(c)){
-			b++;	
-	}
-	if(c==' '){
-	i++;	
-	}
-}
-	printf("El total de numeros es: %d \n", b);
-	printf("El total de palabras es: %d \n", i);
-	fclose(file);
-	return 0;	
-}
+ int main(){
+ char string[100];
+ int i=0;
+  FILE *file;
+  file=fopen("archivo.txt", "r");
+  if (file==NULL){
+  printf("No existe el archivo\n");
+  return 0;
+  }
+ printf("El archivo dice: \n");
+ while(!feof(file)){
+ fgets(string,100,file);	
+ }
+ for(i=0;i<strlen(string);i++){
+ string[i]=toupper(string[i]);
+ file=fopen("archivo.txt", "a");
+ printf("\n%s", string);
+ fprintf(file, "\n%s", string);
+ fclose(file);
+ return 0;	
+  }

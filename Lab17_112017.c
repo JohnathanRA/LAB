ff3205e7a58e713cb27cb22bb62917d8ff524823
JJBlazrey <31145560+JJBlazrey@users.noreply.github.com>
2017-11-25 12:17:01 -0600

    Update Lab17_112017.c

--- Lab17_112017.c
+++ Lab17_112017.c
@@ -2,25 +2,26 @@
 #include<string.h>
 #include<ctype.h>
 
- int main(){
- char string[100];
- int i=0;
-  FILE *file;
-  file=fopen("archivo.txt", "r");
-  if (file==NULL){
-  printf("No existe el archivo\n");
-  return 0;
-  }
- printf("El archivo dice: \n");
- while(!feof(file)){
- fgets(string,100,file);	
- }
- for(i=0;i<strlen(string);i++){
- string[i]=toupper(string[i]);
- }
- file=fopen("archivo.txt", "a");
- printf("\n%s", string);
- fprintf(file, "\n%s", string);
- fclose(file);
- return 0;	
-  }
+int main(){
+	char string[100];
+	int i=0;
+	FILE *file;
+	
+	file=fopen("archivo.txt", "r");
+	if (file==NULL){
+		printf("No existe el archivo\n");
+		return 0;
+	}
+	printf("El archivo dice: \n");
+	while(!feof(file)){
+		fgets(string,100,file);	
+	}
+	for(i=0;i<strlen(string);i++){
+			string[i]=toupper(string[i]);
+	}
+	printf("\n%s", string);
+	file=fopen("archivo.txt", "a");
+	fprintf(file, "\n%s", string);
+	fclose(file);
+	return 0;
+}

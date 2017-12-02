3ed5ab6668da2efec5a96d9ace534f2841896d6a
JohnathanRA <31198069+JohnathanRA@users.noreply.github.com>
2017-12-01 23:42:11 -0600

    Create Practica_13.c

---
+++ Practica_13.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+ int main(){
+ 	FILE *archivo;
+ 	char c, cadena[30];
+ 	int i=0;
+ 	
+ 	archivo=fopen("wiki.txt", "w");
+ 	
+ 	if(archivo==NULL){
+ 		printf("el archivo no existe");
+ 	}
+ 	
+ 	fputs("hola mundo, como han estado", archivo);
+ 	
+ 	fclose(archivo);
+ 	
+ 	archivo=fopen("wiki.txt", "r");
+ 	
+ 	while(!feof(archivo)){
+ 		c=fgetc(archivo);
+ 		if(c!=' '){
+ 			cadena[i]=c;
+ 			i++;
+ 		}
+ 	}
+ 	for(i=strlen(cadena);i>-1;i--){
+ 		printf("%c", cadena[i]);
+	}
+	
+ 	fclose(archivo);
+ 	return 0;
+ }
